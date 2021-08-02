3685e70f0032627071f46ef6b1df981091542b6b
Mor Vainer <moryvan1@gmail.com>
2021-08-02 20:11:23 +0300

    fifth upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,88 @@
+'use strict';
+
 console.log('Starting up');
+function initPage() {
+    renderPortfolios();
+}
+
+function renderPortfolios() {
+    console.log('rendering');
+    var strHtmls = gProjs.map(function (project) {
+
+        return `  <div class="col-md-4 col-sm-6 portfolio-item">
+        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal" onclick="renderModal('${project.id}')">
+          <div class="portfolio-hover">
+            <div class="portfolio-hover-content">
+              <i class="fa fa-plus fa-3x"></i>
+            </div>
+          </div>
+          <img  class="img-fluid" src=${project.url} alt="">
+        </a>
+        <div class="portfolio-caption">
+          <h4>${project.name}</h4>
+          <p class="text-muted">${project.title}</p>
+        </div>
+        </div>`
+    });
+    console.log(strHtmls);
+    $('.projects-gallery').html(strHtmls);
+
+}
+
+function renderModal(projId){
+    console.log('rendering modal');
+    var proj = getProjbyId(projId);
+    console.log('the proj is (renderModal): ', proj);
+    var strHtml = `<div class="modal-dialog">
+      <div class="modal-content">
+        <div class="close-modal" data-dismiss="modal">
+          <div class="lr">
+            <div class="rl"></div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 mx-auto">
+              <div class="modal-body">
+                <!-- Project Details Go Here -->
+                <h2>${proj.name}</h2>
+                <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
+                <img class="img-fluid d-block mx-auto" src=${proj.url} alt="">
+                <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis
+                  dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate,
+                  maiores repudiandae, nostrum, reiciendis facere nemo!</p>
+                <ul class="list-inline">
+                  <li>Date: January 2017</li>
+                  <li>Client: Threads</li>
+                  <li>Category: Illustration</li>
+                </ul>
+                <button class="btn btn-primary" data-dismiss="modal" type="button">
+                    <i class="fa fa-times"></i>
+                    Close Project</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`
+
+  $('.modal').html(strHtml);
+}
+
+function onModal(projId) {// open the modal and input the data
+    var proj = getProjbyId(projId);
+    var elModal = document.querySelector('.modal');
+    // elModal.innerHtml = book.bookName;
+    elModal.querySelector('h2').innerText = proj.name;
+    var elImg = elModal.querySelector('img');
+    elImg.src = proj.url;
+    elModal.hidden = false;
+   
+   
+}
+
+function onSubmit(){
+    var url=`https://mail.google.com/mail/?view=cm&fs=1&to=moryvan1@gmail.com&su=SUBJECT&body=BODY`;
+    window.location.replace(url);
+
+}
---
+++ js/service-portfolio.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var gProjs = [{
+    id: "101",
+    name: "MineSweeper",
+    title: "This is a minesweeper game",
+    desc: "lorem ipsum lorem ipsum lorem ipsum",
+    url: "img/portfolio/minesweeper.png",
+    publishedAt: 1448693940000,
+    labels: ["Matrixes", "keyboard events"],
+},
+{
+    id: "102",
+    name: "MineSweeper2",
+    title: "This is a minesweeper2 game",
+    desc: "lorem ipsum lorem ipsum lorem ipsum",
+    url: "img/portfolio/minesweeper.png",
+    publishedAt: 1448693940002,
+    labels: ["Matrixes", "keyboard events"],
+}
+];
+
+
+
+function getProjbyId(projId) {
+    // debugger;
+    var proj = gProjs.find(function (proj) {//returns the project itself
+        console.log('the proj(getProjbyId2)is: ', proj);
+        return projId === proj.id;
+    });
+    console.log('the proj(getProjbyId)is: ', proj);
+    return proj;
+
+}
